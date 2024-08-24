dc1fed32b25907a66c09212ea259174a937e4b4b
NANITH777 <109669139+NANITH777@users.noreply.github.com>
2024-08-24 16:28:49 +0300

    Mediapipe

--- counter.py
+++ counter.py
@@ -1,2 +1,9 @@
 import cv2
 import mediapipe as mp
+
+mpHands = mp.solutions.hands
+Hands = mpHands.Hands()
+mpDraw = mp.solutions.drawing_utils
+fingersCoordinate=[(8,6),(12,10),(16,14),(20,18)]
+thumbCoordinate=(4,3)
+cap = cv2.VideoCapture(0)

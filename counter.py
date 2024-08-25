a4380af748c8b1c583bc52ec80c5227597e69976
NANITH777 <109669139+NANITH777@users.noreply.github.com>
2024-08-25 21:52:12 +0300

    Update

--- counter.py
+++ counter.py
@@ -15,3 +15,9 @@ while (cap.isOpened()):
     results = Hands.process(converted_image) # Processing Image for Tracking 
     handNo=0
     lmList=[]
+
+    if results.multi_hand_landmarks: # Getting Landmark(location) of Hands if Exists 
+        for id,lm in enumerate(results.multi_hand_landmarks[handNo].landmark):
+            h,w,c=img.shape
+            cx,cy=int(lm.x*w),int(lm.y*h)
+            lmList.append((cx,cy))

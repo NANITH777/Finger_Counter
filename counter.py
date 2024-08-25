2443e389199601f00140597197e76540b20d9978
NANITH777 <109669139+NANITH777@users.noreply.github.com>
2024-08-25 20:57:39 +0300

    Update

--- counter.py
+++ counter.py
@@ -7,3 +7,11 @@ mpDraw = mp.solutions.drawing_utils
 fingersCoordinate=[(8,6),(12,10),(16,14),(20,18)]
 thumbCoordinate=(4,3)
 cap = cv2.VideoCapture(0)
+
+while (cap.isOpened()):
+    upcount=0
+    success , img = cap.read() # reading Frame 
+    converted_image = cv2.cvtColor(img,cv2.COLOR_BGR2RGB) # converting BGR to RGB
+    results = Hands.process(converted_image) # Processing Image for Tracking 
+    handNo=0
+    lmList=[]
